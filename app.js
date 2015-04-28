7a02fe23ae0e23316d659b94dfa235095a9de1ad
rettori <rettori@gmail.com>
2015-04-28 11:40:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World Openshift\n');
+  res.end('Hello World Openshifts\n');
 }).listen(port);
